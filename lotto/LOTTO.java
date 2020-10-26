ecfe41af808df0c775275725997c8c4f265013c0
hoon <nhoon95@gmail.com>
2020-10-26 10:37:59 +0900

    second

--- lotto/LOTTO.java
+++ lotto/LOTTO.java
@@ -1,10 +1,28 @@
 package lotto;
-
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.ArrayList;
 public class LOTTO {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hi");
+		
+		//45°³ ¸¸µé±â
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		for(int i=1; i<=45; i++) {
+			numbers.add(i);
+		}
+		//¼¯±â
+		Collections.shuffle(numbers);
+		
+		
+		//»Ì±â
+		int[] picked = new int[6];
+		for(int i=0; i<6; i++) {
+			picked[i] = numbers.get(i);
+		}
+		
+		System.out.println(Arrays.toString(picked));
+		
 	}
 
 }
